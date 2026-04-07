9c6184d998257a01046c2011c1115b7d351fafbc
vitornetofreitas-glitch <vitor.neto.freitas@escola.pr.gov.br>
2026-04-07 15:15:00 +0000

    Trabalho Finalizado

--- README.md
+++
@@ -1,2 +0,0 @@
-# JavaScript-1
-JavaScript 1: algoritmos de ordenação
---
+++ listaLivros.js
@@ -0,0 +1,28 @@
+const precosLivros = [
+     {
+        titulo: "JavaScript",
+        preco: 29
+        },
+        { 
+            titulo: "Alice no país das maravilhas",
+            preco: 35
+        },
+        {
+             titulo: "Branca de neve",
+             preco: 30
+        },
+        {
+            titulo: "Castelo da Elseve",
+            preco: 40
+        },
+        {  
+             titulo: "Cidades de papel",
+            preco: 15
+        },
+        { 
+              titulo: "Chapeuzinho vermelho",
+            preco: 25
+        }
+    ];
+
+    module.exports = precosLivros;
